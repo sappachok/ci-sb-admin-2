c7bad0a4276047863085783010f7eac0f56ca2d0
Sappachok <suppachok_sin@nstru.ac.th>
2019-03-10 06:47:41 +0700

    update

--- application/views/admin/_templates/topbar-menu.php
+++ application/views/admin/_templates/topbar-menu.php
@@ -154,7 +154,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Johny Rico</span>
                 <img class="img-profile rounded-circle" src="<?php echo base_url("assets/images"); ?>/profile-pic-100-100.jpg">
               </a>
               <!-- Dropdown - User Information -->
